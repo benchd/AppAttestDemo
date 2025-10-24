582287053c18252b64abb0c6f958ccc973cbe42d
David Bench <github@benchmarkcomputing.com>
2025-10-24 11:50:57 -0400

    Initial Commit

---
+++ AppAttestDemo/AppAttestDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppAttestDemoApp.swift
+//  AppAttestDemo
+//
+//  Created by David Bench on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct AppAttestDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
